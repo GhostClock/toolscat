46819786000660b1f3037ca5b4c941a829673a36
GhostClock <zhangyuanlaifen@gmail.com>
2023-05-13 00:16:36 +0800

    ArcoVue、自动导入、自动引入

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,9 +1,16 @@
 <template>
-    <div>Hello World, 这是工具猫的Home 页</div>
+  <div>Hello World, 这是工具猫的Home 页</div>
+  <a-space>
+    <a-button type="primary" size="mini" @click="handleClickMini">Mini</a-button>
+    <a-button type="primary" size="small">Small</a-button>
+    <a-button type="primary">Medium</a-button>
+    <a-button type="primary" size="large">Large</a-button>
+  </a-space>
 </template>
 <script setup>
-
+const handleClickMini = () => {
+  AMessage.info('hello isboyjc, click mini AButton!')
+}
 </script>
 <style lang="less" scoped>
-    
 </style>
